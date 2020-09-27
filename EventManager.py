1ce9876761b6b521688c9e7fa80ae5f22c8e42ea
Alexander Sinitsyn <alexander.sinitsyn@ics-it.ru>
2020-09-28 00:37:44 +0300

    добавлен TODO комментарий

--- EventManager.py
+++ EventManager.py
@@ -2,6 +2,8 @@ import asyncio
 from collections import deque
 
 
+# TODO - нужно добавить возможность удалять задачи по таймауту или же при
+#  определенных обстоятельствах
 class EventManager:
     def __init__(self):
         self.tasks = deque()
