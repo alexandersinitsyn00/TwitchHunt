32049119ef54cb45148cadef9e38c41f0019b0cd
Alexander Sinitsyn <alexander.sinitsyn@ics-it.ru>
2020-10-26 23:48:06 +0300

    функция для валидации канала, на который подписываются

--- Twitch/TwitchDataParser.py
+++ Twitch/TwitchDataParser.py
@@ -6,6 +6,7 @@ import json
 
 AUTH_TOKEN_VALIDATE_BASE_URL = 'https://id.twitch.tv/oauth2/validate'
 STREAM_BASE_URL = 'https://api.twitch.tv/helix/streams'
+CHANNEL_SEARCH_BASE_URL = 'https://api.twitch.tv/helix/search/channels'
 TOKEN = 'fmw74lryx24be48sjcjvwwtlgsn8ke'
 CLIENT_ID = 'gp762nuuoqcoxypju8c569th9wz7q5'
 
@@ -15,6 +16,20 @@ async def is_valid_auth_token(token):
     async with aiohttp.ClientSession() as session:
         async with session.request('GET', AUTH_TOKEN_VALIDATE_BASE_URL, headers=headers) as resp:
             return resp.status == 200
+
+
+async def is_valid_channel(channel_name):
+    headers = {'Authorization': f'Bearer {TOKEN}',
+               'Client-Id': f'{CLIENT_ID}'}
+    params = {'query': channel_name}
+    async with aiohttp.ClientSession() as session:
+        async with session.request('GET', CHANNEL_SEARCH_BASE_URL, params=params, headers=headers) as resp:
+            data = await resp.json()
+            try:
+                validate_to_channel = data["data"][0]["display_name"]
+                return channel_name == validate_to_channel
+            except IndexError:
+                return False
 
 
 async def get_stream_info_for_channel_if_streaming(channel_name):
@@ -38,9 +53,7 @@ async def get_stream_info_for_channel_if_streaming(channel_name):
 
 
 async def caller():
-    print(await is_valid_auth_token('fmw74lryx24be48sjcjvwwtlgsn8ke'))
-    print(await get_stream_info_for_channel_if_streaming('roflo_chelik'))
-    print(await get_stream_info_for_channel_if_streaming('bratishkinoff'))
+    print(await is_valid_channel('nickmercs'))
 
 
 if __name__ == '__main__':
