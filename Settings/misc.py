269c5e9b30719a51c3c7ac7bdf9d2c7aac17169a
Alexander Sinitsyn <alexandersinitsyn00@gmail.com>
2020-10-29 22:30:41 +0300

    Переход на относительные пути

--- Settings/misc.py
+++ Settings/misc.py
@@ -1,4 +1,4 @@
 from Settings.SettingsManager import SettingsManager
 
 # Файл с настройками
-settings = SettingsManager('C:/Users/asinitsyn/Desktop/test/Data/private_settings.json')
+settings = SettingsManager('./Data/private_settings.json')
